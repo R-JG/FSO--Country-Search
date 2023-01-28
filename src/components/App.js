3406e6d20d52d9ef5a57ed0266c372cf5ff458c8
Robin <robin.james.guerrier@gmail.com>
2023-01-28 15:28:57 -0500

    Call api and set state with response.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
+
+    const [allCountryData, setAllCountryData] = useState(null);
+
+    useEffect(() => {
+        axios
+            .get('https://restcountries.com/v3.1/all')
+            .then(response => setAllCountryData(response.data));
+    }, []);
+
+    if (!allCountryData) return console.log('fetching country data...');
+
     return (
         <div className='App' >
-            <header className='search-bar' ></header>
-            <main className='info-container' ></main>
+            <header className='search-bar' >TEST HEADER</header>
+            <main className='info-container' >TEST MAIN</main>
         </div>
     );
 };
