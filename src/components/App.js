90ada1dc1b97451a85a3617d4ec4e75f8d9b39b4
Robin <robin.james.guerrier@gmail.com>
2023-01-28 15:18:02 -0500

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className='App' >
+            <header className='search-bar' ></header>
+            <main className='info-container' ></main>
+        </div>
+    );
+};
+
+export default App;
